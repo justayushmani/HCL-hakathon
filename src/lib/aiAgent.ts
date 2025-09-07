c84fcad86ac16124898ca014746262990a7901c1
ayushmanitiwari931@gmail.com <ayushmanitiwari931@gmail.com>
2025-09-07 14:29:40 +0530

    initial commit

--- src/lib/aiAgent.ts
+++ src/lib/aiAgent.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { config } from '../../config.ts';
+import { config } from '../config.ts';
 
 // Rate limiting helper
 class RateLimiter {
--- src/lib/fileProcessor.ts
+++ src/lib/fileProcessor.ts
@@ -1,5 +1,5 @@
 // File processing utility for reading files locally without storage
-import { config } from '../../config.ts';
+import { config } from '../config.ts';
 
 export interface ProcessedFile {
   id: string;
